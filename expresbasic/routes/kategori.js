2996c1fabaeda1a08af1765ad37987a7382bd5e6
austinlucy <rrm210410@gmail.com>
2026-03-03 13:24:20 +0700

    06 - Update Data

--- expresbasic/routes/kategori.js
+++ expresbasic/routes/kategori.js
@@ -11,6 +11,7 @@ router.get('/', async function(req, res, next){
     })
 })
 
+//Router untuk Store
 router.post('/store', async function(req, res, next){
     try {
         let {nama_kategori} = req.body;
@@ -23,6 +24,30 @@ router.post('/store', async function(req, res, next){
         return res.status(201).json({
             status: true,
             message: 'Data kategori berhasil di tambahkan'
+        })
+    } catch (error) {
+        return res.status(500).json({
+            status: true,
+            message: 'Terjadi kesalahan pada router'
+        })
+    }
+})
+
+//Router untuk Update
+router.patch('/update/:id', async function(req, res, next){
+    try {
+        let id = req.params.id;
+        let {nama_kategori} = req.body;
+
+        let Data = {
+            nama_kategori
+        }
+
+        await Model_Kategori.Update(id, Data);
+
+        return res.status(201).json({
+            status: true,
+            message: 'Data kategori berhasil di perbarui'
         })
     } catch (error) {
         return res.status(500).json({
